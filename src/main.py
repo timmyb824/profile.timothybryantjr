d83711dbd6bc6facfbce59052e378466fd892072
Tim Bryant <timothybryant3@gmail.com>
2024-09-19 11:29:57 -0400

    refactor: use a dictionary to parse all the data instead of individual items

--- src/main.py
+++ src/main.py
@@ -1,27 +1,30 @@
 from flask import Flask, render_template
-from helpers.definitions import ROOT_DIR
+from src.constants import ROOT_DIR
 import configparser
 
 app = Flask(__name__)
 
+
 def parse_config():
     config = configparser.ConfigParser()
     config.read(f"{ROOT_DIR}/personal.ini")
-    name = config.get("personal", "name")
-    location = config.get("personal", "location")
-    # picture = config.get("personal", "picture")
-    linkedin = config.get("personal", "linkedin")
-    github = config.get("personal", "github")
-    resume = config.get("personal", "resume")
-    blog = config.get("personal", "blog")
-    return name, location, linkedin, github, resume, blog
 
-@app.route('/')
+    # Parse personal section
+    personal_data = dict(config["personal"].items())
+
+    projects = {
+        section: dict(config[section].items())
+        for section in config.sections()
+        if section.startswith("project")
+    }
+    return {**personal_data, "projects": projects}
+
+
+@app.route("/")
 def homepage():
-    name, location, linkedin, github, resume, blog = parse_config()
-    return render_template('homepage.html', name=name, location=location, linkedin=linkedin, github=github, resume=resume, blog = blog)
+    data = parse_config()
+    return render_template("homepage.html", **data)
 
 
-if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5001, debug=True)
+if __name__ == "__main__":
     app.run(debug=True)
